39ba092ff01781451422282e9bf96e77545bc790
Ryan Hellyer <ryanhellyer@gmail.com>
2020-03-08 21:47:28 +0100

    Initial code dump

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ramnotifier
+# Ram Notifier
 An Ubuntu desktop RAM notifier
+
+Simply run the .dev installer and run the command `ramnotifier` to receive a notification every 60 seconds if your RAM drops below 1000 MB.
+
+## Future improvements
+It should be loadable via the search thingy, come with an icon and not require a terminal window to stay open and have an option to set how frequently the check is run and at what limit the RAM is set.
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+chmod u+x ramnotifier
+mkdir ramnotifier-1
+cp ramnotifier ramnotifier-1/
+cd ramnotifier-1/
+dh_make -s --createorig
+grep -v makefile debian/rules > debian/rules.new
+mv debian/rules.new debian/rules
+echo ramnotifier usr/bin > debian/install
+echo "1.0" > debian/source/format
+rm debian/*.ex
+debuild -us -uc
+cd ..
+rm ramnotifier-1 -R
+rm ramnotifier_1* -R
+mv ramnotifier-1-1_amd64.deb ramnotifier_v1.deb
+sudo dpkg -i ramnotifier_v1.deb
